90394262af3c873e07e76c9dd11f3009f3abb638
Amirhossein <amirhaghelan@gmail.com>
2022-02-22 22:36:46 +0330

    initial commit

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.loginpage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    String titles[] = {"Login", "Sign Up"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.vp_main);
+        tabLayout = findViewById(R.id.tablayout);
+
+        ViewPagerAdapter  adapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles[position]);
+            }
+        }).attach();
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.loginpage;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1:
+                return new SignUpFragment();
+            default:
+                return new LoginFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+
+}
